75061f3b4f460c7cd623c0e007db739e02d1d0c6
Glenn Morrison <glenn_morrison@chs.net>
2022-04-21 20:38:45 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -129,6 +129,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     bucket-prefix = "GlennGMorrison"
+    Department = "devops"
     Billable = "True"
   }
 }
